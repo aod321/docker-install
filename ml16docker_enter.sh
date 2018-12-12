d8bb8670ec938425581d31d52d8a9ecdcd6b01d3
yinzi <Myinzi123@gmail.com>
2018-12-12 11:26:28 +0800

    new ros files

---
+++ ml16docker_enter.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker start tanway_ml16ros
+docker exec -it tanway_ml16ros /bin/bash
--- ml16docker_init.sh
+++ ml16docker_init.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 curl -fsSL https://raw.githubusercontent.com/aod321/docker-install/master/install.sh -o install.sh
 sh install.sh --mirror
-docker run -it  --rm\
+docker run -it  --name="tanway_ml16ros"\
     -p 5600:5600/udp \
     --user="tanway" \
     --env="DISPLAY" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    tanway/ml16_ros sh -c "roslaunch ml16_view display_ml16.launch"
-~                           
+    tanway/ml16_ros
